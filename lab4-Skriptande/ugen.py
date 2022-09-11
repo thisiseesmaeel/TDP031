ce151b3d3c565a2d2460960434e9cf2a64efae59
Hadi Ansari <hadan326@student.liu.se>
2022-09-11 18:10:18 +0200

    Working on lab4 script 1

--- lab4-Skriptande/ugen.py
+++ lab4-Skriptande/ugen.py
@@ -1,14 +1,41 @@
 import subprocess
-username = "Maghsood"
+import sys
+import random
 
-#subprocess.run(['echo', 'Hello'])
+def create_user(fullname):
+    user = {}
+    user["first name"] = fullname.split()[0]
+    user["last name"] = fullname.split()[1]
+    user["user name"] = (fullname.split()[0][0:3] + fullname.split()[1][:2] + str(random.randint(100, 999))).lower()
+    user["password"] = ""
 
-# If we want to give sudo to the user
-#subprocess.run(['useradd' , '-s', '/bin/bash', '-d', '/home/{}/'.format(username), '-m', '-G', 'sudo',  username])
+    return 
 
-# If we don't want to give sudo to the user
-subprocess.run(['useradd' , '-s', '/bin/bash', '-d', '/home/{}/'.format(username), '-m', username])
+def main():
+    file_name = sys.argv[1]
+    names = []
+    user_names = {}
 
-password = "123456\n123456"
+    with open (file_name, 'r', encoding='latin_1') as f:
+        content = f.read()
+        names.append(content)
 
-subprocess.run(['printf', password, '|', 'passwd', username])
+    for name in names:
+        print(name)
+    
+    username = "Maghsood"
+
+    # If we want to give sudo to the user
+    #subprocess.run(['useradd' , '-s', '/bin/bash', '-d', '/home/{}/'.format(username), '-m', '-G', 'sudo',  username])
+
+    # If we don't want to give sudo to the user
+    # subprocess.run(['useradd' , '-s', '/bin/bash', '-d', '/home/{}/'.format(username), '-m', username])
+
+    # password = "123456\n123456"
+
+    # subprocess.run(['printf', password, '|', 'passwd', username])
+
+
+#==================
+if __name__ == "__main__":
+    main()
