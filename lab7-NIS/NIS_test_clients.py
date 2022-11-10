4f25eea7ad21d34b55b8ac666e376678711812b0
Hadi Ansari <hadan326@student.liu.se>
2022-11-10 10:00:24 +0100

    Added NIS_test_clients.py and removed all __pycache__ and .pytest files

---
+++ lab7-NIS/NIS_test_clients.py
@@ -0,0 +1,38 @@
+import subprocess
+import netifaces as ni
+import re
+
+class TestNISClient:
+    NIS_DOMAIN = 'nis.saysahadan.example.com'
+
+    def test_nis_domain1(self):
+        nis_domain = subprocess.run(['nisdomainname'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        assert nis_domain.strip() == self.NIS_DOMAIN 
+
+    def test_nis_domain2(self):
+        nis_domain = subprocess.run(['cat /etc/defaultdomain'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        assert nis_domain.strip() == self.NIS_DOMAIN
+
+    def test_rpcbind_service(self):
+        status = subprocess.run(['service', 'rpcbind', 'status'])
+        assert status.returncode == 0 # 0 means the service is running 
+
+    def test_ypbind_service(self):
+        status = subprocess.run(['service', 'ypbind', 'status'])
+        assert status.returncode == 0 # 0 means the service is running 
+
+    def test_nis_server(self):
+        nis_server = subprocess.run(['ypwhich'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        assert nis_server.strip() == "server"
+
+    def test_nsswitch_conf_passwd(self):
+        nsswitch_config = subprocess.run(['cat /etc/nsswitch.conf'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        assert re.search(r'\npasswd:.* nis', nsswitch_config) != None
+
+    def test_nsswitch_conf_group(self):
+        nsswitch_config = subprocess.run(['cat /etc/nsswitch.conf'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        assert re.search(r'\ngroup:.* nis', nsswitch_config) != None
+    
+    def test_nsswitch_conf_shadow(self):
+        nsswitch_config = subprocess.run(['cat /etc/nsswitch.conf'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+        assert re.search(r'\nshadow:.* nis', nsswitch_config) != None
