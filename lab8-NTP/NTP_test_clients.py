b80fba232863dbef806675fe0248c4453cc37b7e
Hadi Ansari <hadan326@student.liu.se>
2022-11-22 09:51:02 +0100

    Done with tests for lab8-NTP

--- lab8-NTP/NTP_test_clients.py
+++ lab8-NTP/NTP_test_clients.py
@@ -4,6 +4,7 @@ import netifaces as ni
 
 class TestDNSClient1:
     NTP_server_IP = '10.0.0.1' # gw IP address
+    REASONABLE_OFFSET = 10  # Reasoable offset in millisecond
 
     def test_ntp_service(self):
         status = subprocess.run(['service', 'ntp', 'status'])
@@ -22,3 +23,14 @@ class TestDNSClient1:
         ntp_server = output.split()[0]
         
         assert re.search(r'gw', ntp_server) != None
+
+    # Complete this test
+    def test_response_time(self):
+        proc1 = subprocess.Popen(['ntpq', '-p'], stdout=subprocess.PIPE)
+        proc2 = subprocess.Popen(['grep', "gw"], stdout=subprocess.PIPE, stdin=proc1.stdout)
+        proc1.stdout.close()
+        output, err = proc2.communicate()
+        output = output.decode('utf-8')
+        offset = abs(float(output.split()[-1]))
+
+        assert offset <= self.REASONABLE_OFFSET
