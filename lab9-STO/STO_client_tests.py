c1d13d46915529c310e9c325e484a1c332a024a2
Hadi Ansari <hadan326@student.liu.se>
2022-11-30 16:06:22 +0100

    WIP on lab9-STO

--- lab9-STO/STO_client_tests.py
+++ lab9-STO/STO_client_tests.py
@@ -3,11 +3,14 @@ import re
 import netifaces as ni
 
 class TestSTOServer:
-    CLIENT1_IP = "10.0.0.2"
-    CLIENT2_IP = "10.0.0.3"
-    SERVER_IP = "10.0.0.4"
+    SERVER_REGEX_IP = "10\.0\.0\.4"
 
     def test_verify_upstart_mount_usr_local(self):
         result = subprocess.run(['cat', '/etc/fstab'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        assert re.search(r'\n{}'.format(self.SERVER_IP), result) != None
+        assert re.search(r'\n{}'.format(self.SERVER_REGEX_IP), result) != None
+
+    # Verify that nis is used for auto.master
+    def test_verify_nis(self):
+        result = subprocess.run(['cat', '/etc/nsswitch.conf'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        assert re.search(r'\nautomount:((\s+)|(.* ))nis', result) != None
 
--- lab9-STO/STO_server_tests.py
+++ lab9-STO/STO_server_tests.py
@@ -3,39 +3,26 @@ import re
 import netifaces as ni
 
 class TestSTOServer:
-    CLIENT1_IP = "10.0.0.2"
-    CLIENT2_IP = "10.0.0.3"
-    SERVER_IP = "10.0.0.4"
+    CLIENT1_REGEX_IP = r"10\.0\.0\.2"
+    CLIENT2_REGEX_IP = r"10\.0\.0\.3"
+    SERVER_REGEX_IP = r"10\.0\.0\.4"
+    READ_WRITE_ACCESS = "rw"
 
     # Test for checking if home1 and home2 are exported to the both clients (client1 and client2)
     def test_verify_client_exports(self):
-        result = subprocess.run(['exportfs', '-s'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        assert re.search(r'\n\/home1\s+{}'.format(self.CLIENT1_IP), result) != None
-        assert re.search(r'\n\/home2\s+{}'.format(self.CLIENT1_IP), result) != None
-        assert re.search(r'\n\/home1\s+{}'.format(self.CLIENT2_IP), result) != None
-        assert re.search(r'\n\/home2\s+{}'.format(self.CLIENT2_IP), result) != None
+        result = subprocess.run(['cat', '/etc/exports'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        assert re.search(r'\n/home1\s+{}'.format(self.CLIENT1_REGEX_IP), result) != None
+        assert re.search(r'\n/home2\s+{}'.format(self.CLIENT1_REGEX_IP), result) != None
+        assert re.search(r'\n/home1\s+{}'.format(self.CLIENT2_REGEX_IP), result) != None
+        assert re.search(r'\n/home2\s+{}'.format(self.CLIENT2_REGEX_IP), result) != None
 
-    def test_verfiy_access_rights(self):
-        result = subprocess.run(['cat', '/etc/ntp.conf'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        assert re.search(r'\nserver {}'.format(self.NTP_server_IP), result) != None
-    
-    # def test_gw_respond_to_queries(self):
-    #     proc1 = subprocess.Popen(['ntpq', '-p'], stdout=subprocess.PIPE)
-    #     proc2 = subprocess.Popen(['grep', "gw"], stdout=subprocess.PIPE, stdin=proc1.stdout)
-    #     proc1.stdout.close()
-    #     output, err = proc2.communicate()
-    #     output = output.decode('utf-8')
-    #     ntp_server = output.split()[0]
+    def test_verify_access_rights(self):
+        result = subprocess.run(['cat', '/etc/exports'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        assert re.search(r'(\n|^)\/usr\/local\s+(({}.+?{})|(.*{}.+?{}))'.format(self.CLIENT1_REGEX_IP, self.READ_WRITE_ACCESS, self.CLIENT1_REGEX_IP, self.READ_WRITE_ACCESS), result) != None
+        assert re.search(r'(\n|^)\/usr\/local\s+(({}.+?{})|(.*{}.+?{}))'.format(self.CLIENT2_REGEX_IP, self.READ_WRITE_ACCESS, self.CLIENT2_REGEX_IP, self.READ_WRITE_ACCESS), result) != None
         
-    #     assert re.search(r'gw', ntp_server) != None
-
-    # # Complete this test
-    # def test_response_time(self):
-    #     proc1 = subprocess.Popen(['ntpq', '-p'], stdout=subprocess.PIPE)
-    #     proc2 = subprocess.Popen(['grep', "gw"], stdout=subprocess.PIPE, stdin=proc1.stdout)
-    #     proc1.stdout.close()
-    #     output, err = proc2.communicate()
-    #     output = output.decode('utf-8')
-    #     offset = abs(float(output.split()[-1]))
-
-    #   assert offset <= self.REASONABLE_OFFSET
+        assert re.search(r'(\n|^)\/home1\s+(({}.+?{})|(.*{}.+?{}))'.format(self.CLIENT1_REGEX_IP, self.READ_WRITE_ACCESS, self.CLIENT1_REGEX_IP, self.READ_WRITE_ACCESS), result) != None
+        assert re.search(r'(\n|^)\/home1\s+(({}.+?{})|(.*{}.+?{}))'.format(self.CLIENT2_REGEX_IP, self.READ_WRITE_ACCESS, self.CLIENT2_REGEX_IP, self.READ_WRITE_ACCESS), result) != None
+        
+        assert re.search(r'(\n|^)\/home2\s+(({}.+?{})|(.*{}.+?{}))'.format(self.CLIENT1_REGEX_IP, self.READ_WRITE_ACCESS, self.CLIENT1_REGEX_IP, self.READ_WRITE_ACCESS), result) != None
+        assert re.search(r'(\n|^)\/home2\s+(({}.+?{})|(.*{}.+?{}))'.format(self.CLIENT2_REGEX_IP, self.READ_WRITE_ACCESS, self.CLIENT2_REGEX_IP, self.READ_WRITE_ACCESS), result) != None
